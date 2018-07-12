c514ddf1ffb15c3e0018b35a8fd109cc0b8d785e
gyqgyq <510743998@qq.com>
2018-07-12 17:24:22 +0800

    ladash

--- miao/lodash/gyqgyq-lodash.js
+++ miao/lodash/gyqgyq-lodash.js
@@ -1,5 +1,24 @@
-var gyqgyq = {
-  chunk: function(array, size) {
+var gyqgyq = function() {
 
+  function chunk(array, size = 1) {
+    let res = []
+    let arr = []
+    for (let i of array) {
+      arr.push(i)
+      if (arr.length === size) {
+        res.push(arr)
+        arr = []
+      }
+    }
+    if (arr.length !== 0) {
+      res.push(arr)
+    }
+    return res
   }
-}
+
+
+
+  return {
+    chunk: chunk
+  }
+}()
